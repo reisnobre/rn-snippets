444f66fe1958edab5b99b44bd9cb473817a845ce
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2020-04-02 19:12:30 -0300

    update'

---
+++ tests/javascript.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  routes: [
+    {
+      path: '',
+      name: '',
+      component: 
+    }
+  ]
+})
